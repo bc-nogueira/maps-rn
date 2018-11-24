ab4a493b6f2cceb5e240ce7fb163c587c6da1872
bc-nogueira <bcnogueira@id.uff.br>
2018-11-24 16:40:02 -0200

    Inserindo markers.

--- App.js
+++ App.js
@@ -1,40 +1,69 @@
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import {Alert, StyleSheet} from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      // markers: [],
+      coordinate: {
+        latitude: -22.905837,
+        longitude: -43.133239,
+      }
+    }
+  }
+
+  handlePress = (e) => {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate
+        }
+      ]
+    });
+  }
+
+  // Mantenho essa função aqui caso eu precise depurar
+  exibeAlerta = (msg) => {
+    Alert.alert(
+      'Mensagem',
+      `${msg}`,
+      [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+      { cancelable: true }
+    )
+  }
+
   render() {
     return (
       <MapView
-        style={ styles.map }
+        style={ styles.container }
         initialRegion={{
           latitude: -22.905837,
           longitude: -43.133239,
-          // Acho que os deltas determinam a distância do zoom
+          // Deltas determinam a distância do zoom
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+        // onPress={e => this.handlePress(e)}
+      >
+        {/* {this.state.markers.map((marker, i) => {
+          return <Marker {...marker} key={i} />
+        })} */}
+        <Marker
+          coordinate={this.state.coordinate}
+          title="Teste"
+          description="Testando"
+        />
+      </MapView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    flex: 1,
   }
 });

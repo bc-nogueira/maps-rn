7e0f45c41d7250f7430a9c0e47ab9e869d0dd304
bc-nogueira <bcnogueira@id.uff.br>
2018-11-24 17:22:56 -0200

    Adicionando directions.

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 import {Alert, StyleSheet} from 'react-native';
 
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       // markers: [],
-      coordinate: {
+      origin: {
         latitude: -22.905837,
         longitude: -43.133239,
-      }
+      },
+      destination: {
+        latitude: -22.896679,
+        longitude: -43.124275,
+      },
+      apikey: 'AIzaSyAxFARM9NrPcw8lyIsFrwPRrG1v5kdaKL4'
     }
   }
 
@@ -52,10 +58,14 @@ export default class App extends Component {
         {/* {this.state.markers.map((marker, i) => {
           return <Marker {...marker} key={i} />
         })} */}
-        <Marker
-          coordinate={this.state.coordinate}
-          title="Teste"
-          description="Testando"
+        <Marker coordinate={this.state.origin} />
+        <Marker coordinate={this.state.destination} />
+        <MapViewDirections
+          origin={this.state.origin}
+          destination={this.state.destination}
+          apikey={this.state.apikey}
+          strokeWidth={5}
+          strokeColor="green"
         />
       </MapView>
     );
